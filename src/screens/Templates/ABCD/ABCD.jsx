bb860f7bc2a8f8a718ff3d533c0f51839cc1b5ea
Danil Makarov <ninjaheister@gmail.com>
2020-06-11 13:46:20 +0300

    Templates

--- src/screens/Templates/ABCD/ABCD.jsx
+++ src/screens/Templates/ABCD/ABCD.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import s from './ABCD.module.css'
+import g from './../Style.module.css';
+import Question from '../Objects/Question/Question.jsx';
+import Answer from '../Objects/Answer/Answer.jsx';
 import Strong from "../Icon/Strong";
 import PointDown from "../Icon/PointDown";
 import Lamb from "../Icon/Lamb";
@@ -8,93 +11,90 @@ import CorrectArrow from "../Icon/CorrectArrow";
 import Incorrect from "../Icon/Incorrect";
 import Party from "../Icon/Party";
 const ABCD = () => {
-    return(
-        <div className={s.background}>
-            <div className={s.page}>
-                <div className={s.question_header}>
-                    <div className={s.title_bar}>
-                        <p><strong><Strong/> Практика</strong></p>
-                        <p><strong>українська мова та література </strong> 2018 додаткова сесія</p>
-                        <button className={s.end}>Завершити</button>
+    return (
+        <div className={g.background}>
+            <div className={[s.page, g.page_].join(' ')} >
+                <div className={g.question_header}>
+                    <div className={g.title_bar}>
+                        <p><strong><Strong /> Практика</strong></p>
+                        <div><strong>українська мова та література </strong> 2018 додаткова сесія</div>
+                        <button className={g.end}>Завершити</button>
                     </div>
                 </div>
-            <div className={s.question_body}>
+                <div className={s.question_body}>
+                    <div className={s.question_form}>
+                        <Question />
+                        <div className={g.topic_frame}>
+                            <p><strong><PointDown /> Тема:</strong></p>
+                            <div className={s.topic_frame_text}>Орфомеопія</div>
+                        </div>
+                        <div className={g.comment_frame}>
+                            <p><strong><Lamb /> Коментар</strong></p>
+                            <div className={g.comment_frame_text}>Перед тим як читати варіанти відповідей, спросубй самостійно пояснити вживання розподілових знаків, а вже потім порівнюй їх із запропонованими.</div>
+                        </div>
+                        <div className={g.video_explanation_frame}>
+                            <p><strong><VideoCamera /> Відеопояснення</strong></p>
+                            <div className={g.video}></div>
+                        </div>
+                    </div>
 
-                <div className={s.question_form}>
-                    <div className={s.question_frame}>
-                        <div className={s.title}><strong>Завдання 12</strong></div>
-                        <p>Прочитайте речення (цифра позначає попередній розділовий знак)</p>
-                        <p>Аж тут підвівся Орфей і,(1) ударивши по золотих струнах своєї кіфари,(2) заспівав: то була пісня,(3) що її найдужче любила його мати,(4)прекрасна муза Калліопа, ударивши по золотих струнах своєї кіфари,(5) і,(6) можливо,(7) саме згадка про неї надала Орфеєвому голосу ще більшої сили,(8) щирості,(9) влади.</p>
-                        <p>НЕПРАВИЛЬНО обґрунтовано вживання розділових знаків, запропоноване в рядку</p>
-                    </div>
-                    <div className={s.topic_frame}>
-                        <p><strong><PointDown/> Тема:</strong></p>
-                        <p className={s.topic_frame_text}>Орфомеопія</p>
-                    </div>
-                    <div className={s.comment_frame}>
-                        <p><strong><Lamb /> Коментар</strong></p>
-                        <p className={s.comment_frame_text}>Перед тим як читати варіанти відповідей, спросубй самостійно пояснити вживання розподілових знаків, а вже потім порівнюй їх із запропонованими.</p>
-                    </div>
-                    <div className={s.video_explanation_frame}>
-                        <p><strong><VideoCamera /> Відеопояснення</strong></p>
-                        <div className={s.video}></div>
+                    <div className={s.answers_frame}>
+                        <p className={g.choose}><strong>Обери одну відповідь</strong></p>
+                        <div className={s.answers}>
+                            <Answer />
+                            <div className={g.answer}>
+                                <div className={g.answer_text_frame}>
+                                    <div className={g.letter_choice}>
+
+                                        <div className={g.check}>
+                                            <div className={g.symbol}><Incorrect /></div>
+                                            <div className={g.letter}><strong>Б:</strong></div>
+                                        </div>
+                                    </div>
+                                    <div className={g.answer_text}>коми 4 і 5 — при відокремленій прикладці</div>
+                                </div>
+                                <div className={g.comment}>Пояснення правильне, "прекрасна муза Калліопа" - прикладка, яка відокремлюється комами, бо стоїть після означуваного слова "мати".</div>
+                            </div>
+                            <div className={g.answer}>
+                                <div className={g.answer_text_frame}>
+                                    <div className={g.letter_choice}>
+
+                                        <div className={g.check}>
+                                            <div className={g.symbol}><Incorrect /></div>
+                                            <div className={g.letter}><strong>В:</strong></div>
+                                        </div>
+                                    </div>
+                                    <div className={g.answer_text}>коми 4 і 5 — при відокремленій прикладці</div>
+                                </div>
+                                <div className={g.comment}>Пояснення правильне, "прекрасна муза Калліопа" - прикладка, яка відокремлюється комами, бо стоїть після означуваного слова "мати".</div>
+                            </div>
+                            <div className={g.answer}>
+                                <div className={g.answer_text_frame}>
+                                    <div className={g.letter_choice}>
+
+                                        <div className={g.check}>
+                                            <div className={g.symbol}><Incorrect /></div>
+                                            <div className={g.letter}><strong>Г:</strong></div>
+                                        </div>
+                                    </div>
+                                    <div className={g.answer_text}>коми 4 і 5 — при відокремленій прикладці</div>
+                                </div>
+                                <div className={g.comment}>Пояснення правильне, "прекрасна муза Калліопа" - прикладка, яка відокремлюється комами, бо стоїть після означуваного слова "мати".</div>
+                            </div>
+                        </div>
+                        <div className={g.result_frame}>
+                            <p className={g.result}><strong><Party /> Правильно!</strong></p>
+                            <p className={g.stats}>80% учнів впоралися з цим завданням</p>
+                            <div className={s.wrap}>
+                                <button class={g.btn}>Наступне питання</button>
+                                <button class={g.pass}>Пропустити</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                
-                <div className={s.answers_frame}>
-                    <p className={s.choose}><strong>Обери одну відповідь</strong></p>
-                    <div className={s.answers}>
-                        <div className={s.answer}>
-                            <div className={s.answer_text_frame}>
-                                <div className={s.letter_choice}>
-                                    <div className={s.icon}></div>
-                                    <p><strong>А:</strong></p>
-                                </div>
-                                <p className={s.answer_text}> коми 1 і 2 — при відокремленій обставині</p>
-                            </div>
-                            <p className={s.comment}>Сполука "ударивши по золотих струнах своєї кіфари" — відокремлена обставина, виражена дієприслівниковим зворотом, пояснення правильне.</p>
-                        </div>
-                        <div className={s.answer}>
-                            <div className={s.answer_text_frame}>
-                                <div className={s.letter_choice}>
-                                    <div className={s.icon}></div>
-                                    <p><strong><CorrectArrow /> Б:</strong></p>
-                                </div>
-                                <p className={s.answer_text}> кома 3 – перед сурядною частиною складного речення</p>
-                            </div>
-                            <p className={s.comment}>Кома 3 стоїть перед підрядною означальною частиною складного речення (...то була пісня, (яка?) що її найдужче любила його мати...), тому пояснення є помилковим.</p>
-                        </div>
-                        <div className={s.answer}>
-                            <div className={s.answer_text_frame}>
-                                <div className={s.letter_choice}>
-                                    <div className={s.icon}></div>
-                                    <p><strong><Incorrect /> В:</strong></p>
-                                </div>
-                                <p className={s.answer_text}>коми 4 і 5 — при відокремленій прикладці</p>
-                            </div>
-                            <p className={s.comment}>Пояснення правильне, "прекрасна муза Калліопа" - прикладка, яка відокремлюється комами, бо стоїть після означуваного слова "мати".</p>
-                        </div>
-                        <div className={s.answer}>
-                            <div className={s.answer_text_frame}>
-                                <div className={s.letter_choice}>
-                                    <div className={s.icon}></div>
-                                    <p><strong>Г:</strong></p>
-                                </div>
-                                <p className={s.answer_text}> коми 6 і 7 – при вставному слові</p>
-                            </div>
-                            <p className={s.comment}>"Можливо" - вставне слово, що виражає невпевненість; оскільки вставні слова завжди виділяємо комами, пояснення правильне.</p>
-                        </div>
-                    </div>
-                    <div className={s.result_frame}>
-                        <p className={s.result}><strong><Party /> Правильно!</strong></p>
-                        <p className={s.stats}>80% учнів впоралися з цим завданням</p>
-                        <button class={s.btn}>Наступне питання</button>
-                    </div>
-                </div>
-            </div>
             </div>
         </div>
     )
-} 
+}
 
 export default ABCD;
