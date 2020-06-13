ee081d85fbbf8df78020b7b8d6e4a16ca609d512
Danil Makarov <ninjaheister@gmail.com>
2020-06-14 00:19:27 +0300

    Header

--- src/screens/Templates/Objects/Header/Header.jsx
+++ src/screens/Templates/Objects/Header/Header.jsx
@@ -6,19 +6,19 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-            <div className={g.question_header}>
-                <div className={g.title_bar}>
-                    <p><strong><Strong />Практика</strong></p>
-                    <p><strong>{this.props.subject}</strong> {this.props.year} {this.props.session} сесія</p>
-                    <button className={g.end}>Завершити</button>
+                <div className={g.question_header}>
+                    <div className={g.title_bar}>
+                        <p><strong><Strong />Практика</strong></p>
+                        <p><strong>{this.props.subject}</strong> {this.props.year} {this.props.session} сесія</p>
+                        <button className={g.end}>Завершити</button>
+                    </div>
                 </div>
+                <QuestionNavPanel
+                    list={this.props.list}
+                    callback={this.props.callback}
+                    active={this.props.active}
+                />
             </div>
-            <QuestionNavPanel
-                list={this.props.list}
-                callback={this.props.callback}
-                active={this.props.active}
-            />
-        </div>
         );
     }
 }
