7c2c6282562cca5f1c06e3d8660ea841c64d7ae3
SevkavTV <seva.archakov@gmail.com>
2020-06-16 01:35:29 +0300

    Home page

--- src/screens/Courses/Courses.jsx
+++ src/screens/Courses/Courses.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
+import firebase from './../../global'
 import s from './Courses.module.css';
-import g from './../../Templates/Style.module.css';
+import g from './../Templates/Style.module.css';
 import Course from './Object/Course';
-import Fire from './../../Templates/Icon/Fire/Fire';
-import Moai from './../../Templates/Icon/Moai/Moai';
-import Swords from './../../Templates/Icon/Swords/Swords';
-import Telescope from './../../Templates/Icon/Telescope/Telescope';
-import DNA from './../../Templates/Icon/DNA/DNA';
-import Earth from './../../Templates/Icon/Earth/Earth';
-import German from './../../Templates/Icon/German/German';
-import France from './../../Templates/Icon/France/France';
-import UK from './../../Templates/Icon/UK/UK';
-import Book from './../../Templates/Icon/Book/Book';
+import Fire from './../Templates/Icon/Fire';
+import Moai from './../Templates/Icon/Moai';
+import Swords from './../Templates/Icon/Swords';
+import Telescope from './../Templates/Icon/Telescope';
+import DNA from './../Templates/Icon/DNA';
+import Earth from './../Templates/Icon/Earth';
+import German from './../Templates/Icon/German';
+import France from './../Templates/Icon/France';
+import UK from './../Templates/Icon/UK';
+import Book from './../Templates/Icon/Book'
+import { Redirect } from 'react-router-dom';
 class Courses extends React.Component {
-    
+    state = {
+        user: 25
+    }
+
+    componentDidMount = () => this.getAuthStatus();
+
+    // Get firebase auth status.
+    getAuthStatus = () => {
+        firebase.auth().onAuthStateChanged((resp) => {
+
+            // Pass response to a call back func to update state
+            this.updateUserState(resp);
+        });
+    }
+
+    // update state
+    updateUserState = (resp) => {
+        this.setState({
+            user: resp
+        })
+    }
+
+    navigate = (ref) => {
+        this.props.history.push(ref);
+    }
+
     render() {
         var pic1 = <Book />
         var pic2 = <Moai />
@@ -23,30 +50,38 @@ class Courses extends React.Component {
         var pic6 = <UK />
         var pic7 = <France />
         var pic8 = <German />
-        return (
-            <div className={g.background}>
-                <div className={[s.page, g.page_].join(' ')}>
-                    <div className={g.question_header}>
-                        <div className={s.header}>
-                            <div className={s.title}>Останній ривок до ЗНО <br></br>разом з мавкою <Fire /></div>
-                            <div className={s.description}>Практикуйся, вчися на поясненнях та проходь симуляції екзамену. Обери предмет:</div>
-                        </div>
-                        
-                        <div className={s.courses_wrapper}>
-                            <Course course={'Українська мова і література'} pic={pic1} />
-                            <Course course={'Математика'} pic={pic2} />
-                            <Course course={'Історія України'} pic={pic3}/>
-                            <Course course={'Біологія'} pic={pic4}/>
-                            <Course course={'Географія'} pic={pic5} style={{opacity:0.4, cursor: 'default'}} />
-                            <Course course={'Англійська мова'} pic={pic6} style={{opacity:0.4, cursor: 'default'}}/>
-                            <Course course={'Французька мова'}pic={pic7} style={{opacity:0.4, cursor: 'default'}}/>
-                            <Course course={'Німецька мова'}pic={pic8} style={{opacity:0.4, cursor: 'default'}}/>
+        if(this.state.user == 25){
+            return(<div></div>)
+        }
+        if(this.state.user){
+            return (
+                <div className={g.background}>
+                    <div className={[s.page, g.page_].join(' ')}>
+                        <div className={g.question_header}>
+                            <div className={s.header}>
+                                <div className={s.title}>Останній ривок до ЗНО <br></br>разом з мавкою <Fire /></div>
+                                <div className={s.description}>Практикуйся, вчися на поясненнях та проходь симуляції екзамену. Обери предмет:</div>
+                            </div>
                             
+                            <div className={s.courses_wrapper}>
+                                <Course course={'Українська мова і література'} pic={pic1} navigate={this.navigate}/>
+                                <Course course={'Математика'} pic={pic2} navigate={this.navigate}/>
+                                <Course course={'Історія України'} pic={pic3} navigate={this.navigate}/>
+                                <Course course={'Біологія'} pic={pic4} navigate={this.navigate}/>
+                                <Course course={'Географія'} pic={pic5} style={{opacity:0.4, cursor: 'default'}} />
+                                <Course course={'Англійська мова'} pic={pic6} style={{opacity:0.4, cursor: 'default'}}/>
+                                <Course course={'Французька мова'}pic={pic7} style={{opacity:0.4, cursor: 'default'}}/>
+                                <Course course={'Німецька мова'}pic={pic8} style={{opacity:0.4, cursor: 'default'}}/>
+                                
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
+        else {
+            return(<Redirect to="/login"/>);
+        }
     }
 }
 
