7c52ba354d1ca9b2ed7d83fc5bca5b32b3200c79
Danil Makarov <ninjaheister@gmail.com>
2020-06-15 18:51:08 +0300

    Fixed_Progress_description

--- src/screens/MainMenu/MainMenu.jsx
+++ src/screens/MainMenu/MainMenu.jsx
@@ -171,7 +171,7 @@ class MainMenu extends React.Component {
                                 </div>
                                 <div className={s.skip}>
                                     <button className={g.btn}>Скинути прогрес</button>
-                                    <div className={s.description} >Ти втратиш прогрес, бали і рекомендовані теми, та зможеш практикувати та симулювати цей тест з нуля </div>
+                                    <div className={s.description} style={{width:'130%',fontSize: '14px', lineHeight:'18px'}}>Ти втратиш прогрес, бали і рекомендовані теми, та зможеш практикувати та симулювати цей тест з нуля </div>
                                 </div>
                             </div>) : null}
                         </div>
