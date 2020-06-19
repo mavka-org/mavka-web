2e455d61e6f83e918b7601e6a866c8a89b511c80
Danil Makarov <ninjaheister@gmail.com>
2020-06-19 23:10:32 +0300

    Lol

--- src/screens/NotFound/NotFound.js
+++ src/screens/NotFound/NotFound.js
@@ -1,5 +1,9 @@
 import React from 'react';
-const ScreensNotFound = (
-    <div>Not Found 404</div>
-);
+const ScreensNotFound = () => {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <p><img src='https://i.pinimg.com/originals/e6/7f/67/e67f678b67683359e5cc4f08e2ff4577.gif' width="100%" height="100%"></img></p>
+        </div>
+    )
+}
 export default ScreensNotFound;
