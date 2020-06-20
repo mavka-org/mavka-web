5471376b4d58f5227b41b0322a064a6664a1e55f
Danil Makarov <ninjaheister@gmail.com>
2020-06-21 02:11:27 +0300

    Адаптивный Courses Menu

--- src/screens/Courses/Courses.jsx
+++ src/screens/Courses/Courses.jsx
@@ -20,11 +20,22 @@ import Loading from './../LoadingScreen'
 import LoadingScreen from './../LoadingScreen';
 
 class Courses extends React.Component {
+
+
+
+
+
     state = {
         user: 25,
-        clicked: false
+        clicked: false,
+        width: window.innerWidth,
     }
-
+    updateScreen = this.updateScreen.bind(this);
+    updateScreen() {
+        this.setState({
+            width: window.innerWidth
+        })
+    }
     componentDidMount = () => this.getAuthStatus();
 
     // Get firebase auth status.
@@ -68,6 +79,7 @@ class Courses extends React.Component {
     }
 
     render() {
+        window.addEventListener("resize", this.updateScreen)
         var pic1 = <Book />
         var pic2 = <Moai />
         var pic3 = <Swords />
@@ -84,48 +96,89 @@ class Courses extends React.Component {
             return (<LoadingScreen />);
         }
         if (this.state.user) {
-            return (
-                <div className={g.background}>
-                    {this.customConfirm()}
-                    <div className={[s.page, g.page_].join(' ')}>
-
-                        <div className={s.question_header}>
-
-                            <div className={s.header}>
-                                <div className={s.title}><div className={s.sub_title}>Останній ривок до ЗНО разом з мавкою</div></div>
-                                <div className={s.description}>Практикуйся, вчися на поясненнях та проходь симуляції екзамену. Обери предмет:</div>
-                            </div>
-
-                            {/* <div className={s.exit}>
-                                <button className={s.end} onClick={() => {
+            if (window.innerWidth <= 633) {
+                return (
+                    <div className={g.background}>
+                        {this.customConfirm()}
+                        <div className={[s.page, g.page_].join(' ')}>
+                            <div className={s.header_mobile}>
+                                <div className={s.logo}><strong>мавка</strong> зно</div>
+                                <div className={s.logo} style={{cursor:'pointer'}} onClick={() => {
                                     this.setState({
                                         clicked: true
                                     })
-                                }}>
-                                    Вийти
-                                </button>
-                            </div> */}
-                        </div>
+                                }}><strong>Вихід</strong></div>
 
-                        <div className={s.courses_wrapper}>
-                            <Course hover={true} course={'Українська мова і література'} pic={pic1} navigate={this.navigate} />
-                            <Course hover={true} course={'Математика'} pic={pic2} navigate={this.navigate} />
-                            <Course hover={true} course={'Історія України'} pic={pic3} navigate={this.navigate} />
-                            <Course hover={true} course={'Біологія'} pic={pic4} navigate={this.navigate} />
-                            <Course hover={false} course={'Фізика'} pic={pic5} style={{ opacity: 0.4, cursor: 'default' }} />
-                            <Course hover={false} course={'Географія'} pic={pic6} style={{ opacity: 0.4, cursor: 'default' }} />
-                            <Course hover={false} course={'Англійська мова'} pic={pic7} style={{ opacity: 0.4, cursor: 'default' }} />
-                            <Course hover={false} course={'Французька мова'} pic={pic8} style={{ opacity: 0.4, cursor: 'default' }} />
-                            <Course hover={false} course={'Німецька мова'} pic={pic9} style={{ opacity: 0.4, cursor: 'default' }} />
+                            </div>
+                            <div className={s.header}>
 
-                        </div>
-                        <div style={{ width: '100%', marginTop: '90px', marginBottom: '10px', color: 'Gray' }}><hr></hr></div>
-                        <div className={s.footer}>
-                            <Footer />
+                                <div className={s.title}><div className={s.sub_title}>Останній ривок до ЗНО разом з <div className={s.sub_title_icon}>мавкою <Fire /></div> </div></div>
+                                <div className={s.description}>Практикуйся, вчися на поясненнях та проходь симуляції екзамену. Обери предмет:</div>
+                            </div>
+                            <div className={s.courses_wrapper}>
+                                <Course hover={true} course={'Українська мова і література'} pic={pic1} navigate={this.navigate} />
+                                <Course hover={true} course={'Математика'} pic={pic2} navigate={this.navigate} />
+                                <Course hover={true} course={'Історія України'} pic={pic3} navigate={this.navigate} />
+                                <Course hover={true} course={'Біологія'} pic={pic4} navigate={this.navigate} />
+                                <Course hover={false} course={'Фізика'} pic={pic5} style={{ opacity: 0.4, cursor: 'default' }} />
+                                <Course hover={false} course={'Географія'} pic={pic6} style={{ opacity: 0.4, cursor: 'default' }} />
+                                <Course hover={false} course={'Англійська мова'} pic={pic7} style={{ opacity: 0.4, cursor: 'default' }} />
+                                <Course hover={false} course={'Французька мова'} pic={pic8} style={{ opacity: 0.4, cursor: 'default' }} />
+                                <Course hover={false} course={'Німецька мова'} pic={pic9} style={{ opacity: 0.4, cursor: 'default' }} />
+
+                            </div>
+                            <div style={{ width: '100%', marginTop: '90px', marginBottom: '10px', color: 'Gray' }}><hr></hr></div>
+                            <div className={s.footer}>
+                                <Footer />
+                            </div>
                         </div>
                     </div>
-                </div>
-            );
+                )
+            } else {
+                return (
+                    <div className={g.background}>
+                        {this.customConfirm()}
+                        <div className={[s.page, g.page_].join(' ')}>
+                            <div className={s.question_header}>
+
+                                <div className={s.header}>
+
+                                    <div className={s.title}><div className={s.sub_title}>Останній ривок до ЗНО разом з <div className={s.sub_title_icon}>мавкою <Fire /></div> </div></div>
+                                    <div className={s.description}>Практикуйся, вчися на поясненнях та проходь симуляції екзамену. Обери предмет:</div>
+                                </div>
+
+                                <div className={s.exit}>
+                                    <button className={s.end} onClick={() => {
+                                        this.setState({
+                                            clicked: true
+                                        })
+                                    }}>
+                                        Вийти
+                                        </button>
+                                </div>
+                            </div>
+                            <div className={s.courses_wrapper}>
+                                <Course hover={true} course={'Українська мова і література'} pic={pic1} navigate={this.navigate} />
+                                <Course hover={true} course={'Математика'} pic={pic2} navigate={this.navigate} />
+                                <Course hover={true} course={'Історія України'} pic={pic3} navigate={this.navigate} />
+                                <Course hover={true} course={'Біологія'} pic={pic4} navigate={this.navigate} />
+                                <Course hover={false} course={'Фізика'} pic={pic5} style={{ opacity: 0.4, cursor: 'default' }} />
+                                <Course hover={false} course={'Географія'} pic={pic6} style={{ opacity: 0.4, cursor: 'default' }} />
+                                <Course hover={false} course={'Англійська мова'} pic={pic7} style={{ opacity: 0.4, cursor: 'default' }} />
+                                <Course hover={false} course={'Французька мова'} pic={pic8} style={{ opacity: 0.4, cursor: 'default' }} />
+                                <Course hover={false} course={'Німецька мова'} pic={pic9} style={{ opacity: 0.4, cursor: 'default' }} />
+
+                            </div>
+                            <div style={{ width: '100%', marginTop: '90px', marginBottom: '10px', color: 'Gray' }}><hr></hr></div>
+                            <div className={s.footer}>
+                                <Footer />
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+
+
         }
     }
 }
